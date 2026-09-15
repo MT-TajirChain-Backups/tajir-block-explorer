da56330a5ed4e46bd1d56748f61fd9196c6715d9
Ahtisham <ahtishamul.haq@macromodule.net>
2026-09-15 19:18:30 +0500

    feat: add TypeScript declaration for CSS modules

--- decs.d.ts
+++ decs.d.ts
@@ -2,3 +2,4 @@ declare module 'react-identicons';
 declare module 'use-font-face-observer';
 declare module 'brotli-compress/js';
 declare module '@multisender.app/multisender-react-widget';
+declare module '*.css';
